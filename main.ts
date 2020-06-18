2db86d862749ebb761281c511a5bb85f6d63393a
Horse-1324 <66924482+Horse-1324@users.noreply.github.com>
2020-06-18 13:28:13 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+
+// This function prints out you name and favorite color.
+function nameAndColor(name: string, color: string) {
+    console.log("Hello " + name + ", your favorite color is " + color + ".")
+}
+
+nameAndColor("Shreya", "teal")
+
+
+// This function prints out you name and favorite number.
+function nameAndNumber(name:string, Numero:number){
+    console.log("Hello " + name + ", your favorite number is " + Numero + ".")
+} 
+
+nameAndNumber("Shreya", 3)
+
+
